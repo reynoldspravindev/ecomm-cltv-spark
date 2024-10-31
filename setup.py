e34c1fff31b0111f887300539b52c91808faf4f4
Reynolds Pravindev <70562125+reynoldspravindev@users.noreply.github.com>
2024-10-31 11:54:21 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,7 @@ setuptools.setup(
     version="1.0.0",
     author="Reynolds Pravindev",
     package_dir={"": "cltv"},
-    packages=setuptools.find_packages(where="cltv")
+    packages=setuptools.find_packages(where="cltv"),
+    install_requires=['ucimlrepo'],
+    entry_points={'console_scripts': ['adf_rest_api_cli=adf_rest_api.main:main']},
 )
